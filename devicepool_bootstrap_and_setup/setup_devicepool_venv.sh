bd937e81f3074aaa0fea4e5d40d2309b2ec78e7c
Andrew Erickson <aerickson@mozilla.com>
2019-06-13 16:31:49 -0700

    devicepool_bootstrap: note in venv setup script

--- devicepool_bootstrap_and_setup/setup_devicepool_venv.sh
+++ devicepool_bootstrap_and_setup/setup_devicepool_venv.sh
@@ -4,6 +4,7 @@ set -e
 # set -x
 
 cd /home/bitbar/mozilla-bitbar-devicepool
+# TODO: if it already exists, don't run next bits
 virtualenv venv
 . venv/bin/activate
 pip install -r requirements.txt
