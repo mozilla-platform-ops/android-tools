3f0de25bbfa2b05d75720f3caab4be1569d474c0
Andrew Erickson <aerickson@mozilla.com>
2019-06-13 16:13:52 -0700

    last_started: add daemon mode, run black formatter

--- devicepool_last_started_alert/last_started_alert.py
+++ devicepool_last_started_alert/last_started_alert.py
@@ -9,6 +9,8 @@ import pprint
 import re
 import socket
 import subprocess
+import sys
+import time
 from urllib.request import urlopen
 
 import toml
@@ -17,6 +19,7 @@ from pdpyras import EventsAPISession
 # configuration option-esque constants
 MINUTES_OF_LOGS_TO_INSPECT = 30
 MINIMUM_LINES_OF_JOURNALCTL_OUTPUT = 10
+DAEMON_MODE_CHECK_FREQUENCY_SECONDS = 5 * 60
 PD_SERVICE_ID = "PAYN6NV"
 
 # constants
@@ -58,12 +61,13 @@ class LastStarted:
         #   in progress
 
         # check that it's present
-        if 'PAGERDUTY_TOKEN' in os.environ:
+        if "PAGERDUTY_TOKEN" in os.environ:
             # check that it's non-empty
-            if os.environ['PAGERDUTY_TOKEN']:
-                api_token = os.environ['PAGERDUTY_TOKEN']
+            if os.environ["PAGERDUTY_TOKEN"]:
+                api_token = os.environ["PAGERDUTY_TOKEN"]
                 # TODO: rename to pd_event_session
                 self.pd_session = EventsAPISession(api_token)
+                # TODO: fire an api request and see that it works before setting alerting_enabled
                 self.alerting_enabled = True
                 self.debug(self.pd_session)
 
@@ -89,7 +93,6 @@ class LastStarted:
             count += json_result["pendingTasks"]
         return count
 
-
     # returns boolean, True if there is already an alert created by this program
     # def open_incidents_already(self):
     #     # https://api.pagerduty.com/incidents?statuses%5B%5D=triggered&statuses%5B%5D=acknowledged&service_ids%5B%5D=PS9CADW&time_zone=UTC
@@ -109,18 +112,18 @@ class LastStarted:
     def set_currently_alerting(self, currently_alerting=True):
         if currently_alerting == False:
             self.set_dedup_key("")
-        self.state_dict['alert_state']['currently_alerting'] = currently_alerting
+        self.state_dict["alert_state"]["currently_alerting"] = currently_alerting
         self.write_toml(self.state_dict)
 
     def set_dedup_key(self, key):
-        self.state_dict['alert_state']['current_dedup_key'] = key
+        self.state_dict["alert_state"]["current_dedup_key"] = key
         self.write_toml(self.state_dict)
 
     # TODO: get rid of need for get_new_if_not_set arg
     #       - add a new function - get_or_create_dedup_key(). cleaner?
     def get_dedup_key(self, get_new_if_not_set=True):
         try:
-            dk = self.state_dict['alert_state']['current_dedup_key']
+            dk = self.state_dict["alert_state"]["current_dedup_key"]
         except KeyError:
             # should we ever be here?
             raise Exception("shouldn't ever be here with default toml")
@@ -129,12 +132,12 @@ class LastStarted:
             if get_new_if_not_set:
                 dk = self.create_dedup_key()
             else:
-                raise Exception('not in incident, why are we calling resolve?')
+                raise Exception("not in incident, why are we calling resolve?")
         return dk
 
     def currently_alerting(self):
         try:
-            return self.state_dict['alert_state']['currently_alerting']
+            return self.state_dict["alert_state"]["currently_alerting"]
         except KeyError:
             return False
 
@@ -149,11 +152,16 @@ class LastStarted:
         #   https://v2.developer.pagerduty.com/docs/send-an-event-events-api-v2
         #   https://pagerduty.github.io/pdpyras/#pdpyras.EventsAPISession.trigger
 
-        summary = "Jobs are present in queues, but no 'started' lines in devicepools logs for %s minutes!" % MINUTES_OF_LOGS_TO_INSPECT
+        summary = (
+            "Jobs are present in queues, but no 'started' lines in devicepools logs for %s minutes!"
+            % MINUTES_OF_LOGS_TO_INSPECT
+        )
         hostname = socket.gethostname()
         dedup_key = self.get_dedup_key()
 
-        self.pd_session.trigger(summary, hostname, dedup_key=dedup_key, severity='critical')
+        self.pd_session.trigger(
+            summary, hostname, dedup_key=dedup_key, severity="critical"
+        )
         self.set_dedup_key(dedup_key)
         self.set_currently_alerting()
 
@@ -184,7 +192,10 @@ class LastStarted:
         return False
 
     def get_journalctl_output(self):
-        cmd = "sudo journalctl -u bitbar --since '%s minutes ago'" % MINUTES_OF_LOGS_TO_INSPECT
+        # NOTE: user running needs to be in adm group to not need sudo
+        cmd = (
+            "journalctl -u bitbar --since '%s minutes ago'" % MINUTES_OF_LOGS_TO_INSPECT
+        )
         res = self.run_cmd(cmd)
 
         # TODO: ensure that the process has been running for x minutes before being able to trigger alert?
@@ -206,7 +217,7 @@ class LastStarted:
             raise Exception("non-zero code returned")
 
     def write_toml(self, dict_to_write):
-        with open(STATE_FILE, 'w') as writer:
+        with open(STATE_FILE, "w") as writer:
             toml.dump(dict_to_write, writer)
 
     def enough_journalctl_lines(self):
@@ -231,57 +242,70 @@ current_dedup_key = ""
             self.write_toml(return_dict)
             return return_dict
 
+    def perform_check(self, args):
+        currently_alerting = self.currently_alerting()
+        jobs_in_queues = self.jobs_in_queues()
+        started_lines_present = self.started_lines_present()
+        completed_lines_present = self.completed_lines_present()
+        enough_journalctl_lines = self.enough_journalctl_lines()
+
+        # INFO
+        if args.verbose:
+
+            print("Currently alerting?: %s" % currently_alerting)
+            print("Jobs in queues: %s" % jobs_in_queues)
+            print("Minutes of logs inspected: %s" % MINUTES_OF_LOGS_TO_INSPECT)
+            print("Lines of journalctl output: %s" % self.journalctl_lines_of_output)
+            print("Enough lines of journalctl output?: %s" % enough_journalctl_lines)
+            print("Started lines present?: %s" % started_lines_present)
+            print("Completed lines present?: %s" % completed_lines_present)
+            print("")
+
+        # ALERTING
+        if self.alerting_enabled:
+            # TESTING
+            # dk = self.create_dedup_key()
+            # self.set_dedup_key(dk)
+            # print(self.get_dedup_key())
+            # self.set_currently_alerting()
+            #
+            # self.trigger_event()
+            # self.resolve_incident()
+
+            if enough_journalctl_lines:
+                if jobs_in_queues:
+                    if not started_lines_present:
+                        print("*** Alert conditions met! Sending trigger event.")
+                        self.trigger_event()
+                    else:
+                        if currently_alerting:
+                            print(
+                                "*** Alert conditions not met. Resolving current incident."
+                            )
+                            self.resolve_incident()
+                        else:
+                            print("*** Alert conditions not met.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("-d", "--daemon-mode", action="store_true")
     args = parser.parse_args()
 
     ls = LastStarted()
-
-    currently_alerting = ls.currently_alerting()
-    jobs_in_queues = ls.jobs_in_queues()
-    started_lines_present = ls.started_lines_present()
-    completed_lines_present = ls.completed_lines_present()
-    enough_journalctl_lines = ls.enough_journalctl_lines()
-
-    # INFO
-    if args.verbose:
-
-        print("Currently alerting?: %s" % currently_alerting)
-        print("Jobs in queues: %s" % jobs_in_queues)
-        print("Minutes of logs inspected: %s" % MINUTES_OF_LOGS_TO_INSPECT)
-        print("Lines of journalctl output: %s" % ls.journalctl_lines_of_output)
-        print("Enough lines of journalctl output?: %s" % enough_journalctl_lines)
-        print("Started lines present?: %s" % started_lines_present)
-        print("Completed lines present?: %s" % completed_lines_present)
-        print("")
-
-    # ALERTING
     if ls.alerting_enabled:
-        if args.verbose:
-            print("Alerting is enabled.")
-
-        # TESTING
-        # dk = ls.create_dedup_key()
-        # ls.set_dedup_key(dk)
-        # print(ls.get_dedup_key())
-        # ls.set_currently_alerting()
-        #
-        # ls.trigger_event()
-        # ls.resolve_incident()
-
-        if enough_journalctl_lines:
-            if jobs_in_queues:
-                if not started_lines_present:
-                    print("*** Alert conditions met! Sending trigger event.")
-                    ls.trigger_event()
-                else:
-                    if currently_alerting:
-                        print("*** Alert conditions not met. Resolving current incident.")
-                        ls.resolve_incident()
-                    else:
-                        print("*** Alert conditions not met.")
-
+        print("Alerting is enabled.")
     else:
         print("Alerting is _not_ enabled.")
+
+    if args.daemon_mode:
+        print(
+            "Daemon mode activated. Will perform checks every %s seconds."
+            % DAEMON_MODE_CHECK_FREQUENCY_SECONDS
+        )
+        while True:
+            ls.perform_check(args)
+            time.sleep(DAEMON_MODE_CHECK_FREQUENCY_SECONDS)
+    else:
+        ls.perform_check(args)
