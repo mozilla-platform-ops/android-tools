085642f6d67c5e2c90ba6139a28b229e0bcabc80
Andrew Erickson <aerickson@mozilla.com>
2019-07-23 15:26:41 -0700

    testing: make list unordered

--- worker_health/worker_health_test.py
+++ worker_health/worker_health_test.py
@@ -17,7 +17,7 @@ def test_flatten_list(wh_instance):
   assert output == [1, 2, 1, 2, 3, 4]
 
 def test_dict_merge_with_dedupe(wh_instance):
-  a = {'a': [1, 2, 3], 'b': [4, 5, 6]}
+  a = {'a': [3, 1, 2, 3], 'b': [4, 5, 6]}
   b = {'a': [1, 3], 'b': [9]}
   c = {'a': [1, 2, 3], 'b': [4, 5, 6, 9]}
   result = wh_instance.dict_merge_with_dedupe(a, b)
