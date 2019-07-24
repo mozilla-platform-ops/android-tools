01f6ca170f1c7291aa93cb97a7e7c9949f750583
Andrew Erickson <aerickson@mozilla.com>
2019-07-23 17:34:57 -0700

    ugh

--- worker_health/worker_health.py
+++ worker_health/worker_health.py
@@ -659,7 +659,6 @@ class WorkerHealth:
 
         print("quarantined: %s" % self.quarantined_workers)
         print("missing: %s" % missing_workers)
-        print("missing_no_q: %s" % missing_workers_no_q)
         print("offline: %s" % offline_workers)
         print("")
         print("merged: %s" % merged)
