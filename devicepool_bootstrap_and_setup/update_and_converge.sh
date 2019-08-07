70fff2805554e37fc0b5765fbb43aacf97bc63a3
Andrew Erickson <aerickson@mozilla.com>
2019-08-07 13:30:35 -0700

    bootstrap scripts: add script for use after initial bootstrap

--- devicepool_bootstrap_and_setup/update_and_converge.sh
+++ devicepool_bootstrap_and_setup/update_and_converge.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-# set -x
+set -x
 
 bootstrap_script_path="$HOME/git/ronin_puppet/provisioners/linux/bootstrap_bitbar_devicepool.sh"
 bitbar_env_file_path="$HOME/git/mozilla-bitbar-devicepool/bitbar_env.sh"
@@ -21,42 +21,27 @@ fi
 export the_host="$1"
 
 if [ -z "$the_host" ]; then
-  echo "ERROR: Please provide a host to bootstrap (like devicepool-0.relops.mozops.net)"
+  echo "ERROR: Please provide a host to update (like devicepool-0.relops.mozops.net)"
   exit 1
 fi
 
+#
+# UPDATE THINGS NOT DONE BY PUPPET
+#
+echo "updating things..."
+ssh "$the_host" 'sudo -iu bitbar bash -c "cd ~bitbar/android-tools && git pull --rebase"'
+ssh "$the_host" 'sudo -iu bitbar bash -c "cd ~bitbar/mozilla-bitbar-devicepool && git pull --rebase"'
+# TODO: pipenv install?
 
 #
-# BOOTSTRAP
+# RUN PUPPET
 #
-
-# TODO: detect which phase we're in, don't rerun first phase if we don't need to
-set +e
-ssh relops@"$the_host" id > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  echo "performing first bootstrap phase"
-  # TODO: add "-o StrictHostKeyChecking=no"?
-  scp "$bootstrap_script_path" relops@"$the_host":/tmp/
-  # run first as bootstrap user (relops)
-  ssh relops@"$the_host" sudo /tmp/bootstrap_bitbar_devicepool.sh || true
-else
-  echo "host does not have a relops user, skipping first bootstrap phase..."
-fi
-set -e
 
 # now run as current user and remove relops user (fails in prior run)
-echo "performing second bootstrap phase"
+echo "running puppet..."
 scp "$bootstrap_script_path" "$the_host":/tmp/
 ssh "$the_host" sudo /tmp/bootstrap_bitbar_devicepool.sh
 
-#
-# SETUP
-#
-
-scp "$bitbar_env_file_path" "$the_host":bitbar.env
-ssh "$the_host" 'sudo mv bitbar.env /etc/bitbar/ && sudo chown root:bitbar /etc/bitbar/bitbar.env && sudo chmod 660 /etc/bitbar/bitbar.env'
-
-# venvs are now created in puppet
 
 cat << 'EOF'
 
