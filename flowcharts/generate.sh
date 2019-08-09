d5d6a2a09d9f50681b5a8db4b18a2653bcd09c50
Andrew Erickson <aerickson@mozilla.com>
2019-08-09 10:51:50 -0700

    add new bitbar connection graph (#4)

--- flowcharts/generate.sh
+++ flowcharts/generate.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-input_file="android_hw_gw_execution_flow.dot"
-output_prefix="android_hw_gw_execution_flow"
-
 # cleanup
 # rm *.png *.svg
 
 # generate
-dot -Tsvg $input_file -o $output_prefix.svg
-dot -Tpng $input_file -o $output_prefix.png
+for file in $(ls *.dot)
+do
+  basename=$(basename -s .dot $file)
+  set -x
+  dot -Tsvg $basename.dot -o $basename.svg
+  dot -Tpng $basename.dot -o $basename.png
+  set +x
+done
