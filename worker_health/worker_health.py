078df6e173abf99bb2369b304a59872dfbf6e8e9
Andrew Erickson <aerickson@mozilla.com>
2019-10-07 11:16:53 -0700

    remove merge dict code

--- worker_health/worker_health.py
+++ worker_health/worker_health.py
@@ -130,28 +130,6 @@ class WorkerHealth:
         open(last_updated_file, "a").close()
         os.utime(last_updated_file, None)
 
-    # Merge dictionaries and keep values of common keys in list
-    def mergeDict(self, dict1, dict2):
-        dict3 = {**dict1, **dict2}
-        for key, value in dict3.items():
-            if key in dict1 and key in dict2:
-                    dict3[key] = [value , dict1[key]]
-
-        return dict3
-
-    def mergeDict2(self, dict1, dict2):
-        from itertools import chain
-        from collections import defaultdict
-        # dict1 = {'bookA': 1, 'bookB': 2, 'bookC': 3}
-        # dict2 = {'bookC': 2, 'bookD': 4, 'bookE': 5}
-        dict3 = defaultdict(list)
-        for k, v in chain(dict1.items(), dict2.items()):
-            dict3[k].append(v)
-
-        # for k, v in dict3.items():
-        #     print(k, v)
-        return dict3
-
     # handles continuationToken
     def get_jsonc(self, an_url):
         output_dict = {}
