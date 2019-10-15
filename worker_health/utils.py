b23d483bb144530e7c1cbbf68635f7482cc5b1ac
Andrew Erickson <aerickson@mozilla.com>
2019-10-15 15:41:42 -0700

    wip

--- worker_health/utils.py
+++ worker_health/utils.py
@@ -1,7 +1,11 @@
+import json
+import pprint
+import requests
 import subprocess
 
 from worker_health import logger
 
+USER_AGENT_STRING = "Python (https://github.com/mozilla-platform-ops/android-tools/tree/master/worker_health)"
 
 def run_cmd(cmd):
     return (
@@ -23,3 +27,42 @@ def bitbar_systemd_service_present(warn=False, error=False):
             logger.error("this must be run on the primary devicepool host!")
         return False
     return True
+
+# handles continuationToken
+def get_jsonc(an_url, verbosity=0):
+    output_dict = {}
+    headers = {"User-Agent": USER_AGENT_STRING}
+    retries_left = 2
+
+    while retries_left >= 0:
+        if verbosity > 2:
+            print(an_url)
+        response = requests.get(an_url, headers=headers)
+        result = response.text
+        try:
+            output = json.loads(result)
+            # will only break on good decode
+            break
+        except json.decoder.JSONDecodeError as e:
+            logger.warning("json decode error. input: %s" % result)
+            if retries_left == 0:
+                raise e
+        retries_left -= 1
+    output_dict = output
+
+    while "continuationToken" in output:
+        payload = {"continuationToken": output["continuationToken"]}
+        if verbosity > 2:
+            print("CONT %s, %s" % (an_url, output["continuationToken"]))
+        response = requests.get(an_url, headers=headers, params=payload)
+        result = response.text
+        output = json.loads(result)
+        # tc messes with us and sends back and empty workers array
+        if 'workers' in output and len(output['workers']):
+            # we never hit this...
+            logger.error("shouldn't be here")
+            output_dict = output
+
+    if verbosity > 2:
+            pprint.pprint(output_dict)
+    return output_dict
