2830de1ea2b6d99dfb4102e87877b5bb218ef045
Andrew Erickson <aerickson@mozilla.com>
2020-02-12 17:21:16 -0800

    detect if we should use proxy

--- bitbar_docker_build_tools/build_and_run.sh
+++ bitbar_docker_build_tools/build_and_run.sh
@@ -5,6 +5,30 @@ set -x
 
 ./cleanup.sh
 
+# see if polipo is up
+status=0
+nc -v -z localhost 8123 || status=$?
+ 
+if [ "$status" == 0 ] ; then
+	# polipo is running
+	echo "* using proxy"
+	echo ""
+
+	proxy_host=host.docker.internal
+	export http_proxy="http://localhost:8123"
+	export https_proxy="http://localhost:8123"
+
+	docker build --build-arg http_proxy=http://$proxy_host:8123 \
+		--build-arg https_proxy=http://$proxy_host:8123 \
+		-t test-docker .
+else
+	# polipo is not running
+	echo "* not using proxy"
+	echo ""
+
+	docker build -t test-docker .
+fi
+
 proxy_host=host.docker.internal
 export http_proxy="http://localhost:8123"
 export https_proxy="http://localhost:8123"
