c8e50f4b35aac49dc80d8d20088a0c5bdde11497
Andrew Erickson <aerickson@mozilla.com>
2020-02-18 18:15:14 -0800

    open shell mounts ./tests if it exists

--- bitbar_docker_build_tools/open_shell.sh
+++ bitbar_docker_build_tools/open_shell.sh
@@ -4,12 +4,22 @@ set -e
 
 # docker run -it --entrypoint '/bin/bash' test-docker
 
+root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [ -d "$root_dir/tests" ]; then
+  TESTDIR_MOUNTPOINT="-v $root_dir/tests:/tests"
+fi
 
+# TODO: mount mozilla source in
+# -v $HOME/hg/mozilla-source:/source
+
+# scrapyard
+#
 # -e GENERIC_WORKER_CONF='generic-worker'
   # -e LIVELOG_SECRET='abc123' \
     # --tmpfs /builds \
 #  -p 5037:5037 \
+
 docker run -u root \
   --expose 5037 \
   -e DEVICE_NAME='aje-test' \
@@ -20,5 +30,6 @@ docker run -u root \
   -e gecko_t_ap_test_g5="SECRET_SECRET_SECRET_DO NOT LEAK 1" \
   -e TESTDROID_APIKEY="SECRET_SECRET_SECRET_DO NOT LEAK 2" \
   -it --entrypoint '/bin/bash' \
+  $TESTDIR_MOUNTPOINT \
   test-docker
   
