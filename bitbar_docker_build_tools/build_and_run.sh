7dc5c35bee5523800f554be81b872a8be4a0bc19
Andrew Erickson <aerickson@mozilla.com>
2020-02-25 12:54:48 -0800

    docker build: cleanup extra stuff

--- bitbar_docker_build_tools/build_and_run.sh
+++ bitbar_docker_build_tools/build_and_run.sh
@@ -29,18 +29,6 @@ else
 	docker build -t test-docker .
 fi
 
-proxy_host=host.docker.internal
-export http_proxy="http://localhost:8123"
-export https_proxy="http://localhost:8123"
-
-docker build --build-arg http_proxy=http://$proxy_host:8123 \
-	--build-arg https_proxy=http://$proxy_host:8123 \
-	-t test-docker .
-
-#docker build -t test-docker .
 docker create --name test-docker test-docker
 
 ./open_shell.sh
-
-# sleep 5
-# docker logs test-docker
