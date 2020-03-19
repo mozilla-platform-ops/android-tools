ae5645c528a85232f0ce05a0e3f109f3c6282f58
Andrew Erickson <aerickson@mozilla.com>
2020-03-18 17:50:28 -0700

    add get_quarantined tool

---
+++ quarantine_tools/tc_api_driver.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import taskcluster
+import json
+import os
+import sys
+
+#
+# docs: https://pypi.org/project/taskcluster/
+#
+
+# don't need to create separate credentials
+# per tomprince: 
+#   You can use taskcluster signin to get creds (and can use -s <scope>, possibly multiple times, to restrict scopes).
+
+
+
+
+
+
+with open(os.path.expanduser('~/.tc_quarantine_token')) as json_file:
+    data = json.load(json_file)
+creds = {"clientId": data['clientId'],
+         "accessToken": data['accessToken']}
+
+queue = taskcluster.Queue(
+    {"rootUrl": "https://firefox-ci-tc.services.mozilla.com", "credentials": creds}
+)
+
+import ipdb
+ipdb.set_trace()
+
+sys.exit()
+
+hosts_to_quarantine = []
+packet_hosts_to_quarantine = [4, 6, 10, 13, 14, 16, 43, 53, 54, 65, 68]
+
+for h in packet_hosts_to_quarantine:
+  hosts_to_quarantine.append("machine-%s" % h)
+
+for a_host in hosts_to_quarantine:
+  queue.quarantineWorker(
+      "terraform-packet",
+      "gecko-t-linux",
+      "packet-sjc1",
+      a_host,
+      {"quarantineUntil": taskcluster.fromNow("10 year")},
+  )
