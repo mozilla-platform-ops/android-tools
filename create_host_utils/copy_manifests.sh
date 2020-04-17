1780a9ca750cb93ef16d965e3856977474891987
Andrew Erickson <aerickson@mozilla.com>
2020-04-16 18:15:58 -0700

    create_host_utils: be more verbose in copy_manifests

--- create_host_utils/copy_manifests.sh
+++ create_host_utils/copy_manifests.sh
@@ -12,21 +12,27 @@ set -e
 mc_dir="$MC_CLIENT_PATH"
 
 if [ -d ./hu_i686_* ]; then
+  set -x
   cp \
     ./hu_i686_*/manifest.tt \
     $mc_dir/testing/config/tooltool-manifests/linux32/hostutils.manifest
+  set +x
 fi
 
 if [ -d ./hu_x86_64_* ]; then
+  set -x
   cp \
     ./hu_x86_64_*/manifest.tt \
     $mc_dir/testing/config/tooltool-manifests/linux64/hostutils.manifest
+  set +x
 fi
 
 if [ -d ./hu_mac_* ]; then
+  set -x
   cp \
     ./hu_mac_*/manifest.tt \
     $mc_dir/testing/config/tooltool-manifests/macosx64/hostutils.manifest
+  set +x
 fi
 
 echo "success"
