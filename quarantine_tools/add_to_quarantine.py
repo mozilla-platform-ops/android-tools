50105938fdfd72fcd86f37772105081e677dc871
Andrew Erickson <aerickson@mozilla.com>
2020-04-27 13:13:44 -0700

    quarantine tools: use py3

--- quarantine_tools/add_to_quarantine.py
+++ quarantine_tools/add_to_quarantine.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import taskcluster
 import json
@@ -13,8 +13,8 @@ import sys
 # per tomprince: 
 #   You can use taskcluster signin to get creds (and can use -s <scope>, possibly multiple times, to restrict scopes).
 
-print("requires manual configuration currently. edit source and rerun.")
-sys.exit()
+#print("requires manual configuration currently. edit source and rerun.")
+#sys.exit()
 
 with open(os.path.expanduser('~/.tc_quarantine_token')) as json_file:
     data = json.load(json_file)
@@ -26,7 +26,19 @@ queue = taskcluster.Queue(
 )
 
 hosts_to_quarantine = []
-packet_hosts_to_quarantine = [4, 6, 10, 13, 14, 16, 43, 53, 54, 65, 68]
+
+# 4/20
+# round  1
+#packet_hosts_to_quarantine = [15, 1, 53, 29, 14]
+# round 2
+#packet_hosts_to_quarantine = [7, 2, 37, 46, 28, 45, 22, 64]
+# round 3
+#packet_hosts_to_quarantine = [68]
+# round 4
+# packet_hosts_to_quarantine = [49, 23, 13, 6]
+# round 5
+packet_hosts_to_quarantine = [68]
+
 
 for h in packet_hosts_to_quarantine:
   hosts_to_quarantine.append("machine-%s" % h)
--- quarantine_tools/remove_from_quarantine.py
+++ quarantine_tools/remove_from_quarantine.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import taskcluster
 import json
@@ -13,8 +13,8 @@ import sys
 # per tomprince: 
 #   You can use taskcluster signin to get creds (and can use -s <scope>, possibly multiple times, to restrict scopes).
 
-print("requires manual configuration currently. edit source and rerun.")
-sys.exit()
+# print("requires manual configuration currently. edit source and rerun.")
+# sys.exit()
 
 with open(os.path.expanduser('~/.tc_quarantine_token')) as json_file:
     data = json.load(json_file)
@@ -25,8 +25,23 @@ queue = taskcluster.Queue(
     {"rootUrl": "https://firefox-ci-tc.services.mozilla.com", "credentials": creds}
 )
 
-packet_hosts_to_unquarantine = [49, 23, 13, 6]
 hosts_to_unquarantine = []
+
+# 4/20 incident round 1
+# packet_hosts_to_unquarantine = [1, 14, 15, 2, 22, 28, 29, 33]
+# round 2
+#packet_hosts_to_unquarantine = [13, 23, 44, 6]
+# round 3: can't view logs... unknown
+#packet_hosts_to_unquarantine = [64, 53]
+# round 4
+# 49, 68 insufficient bogomips
+# 7 fetch issues
+# 45, 46 can't view logs (expired)
+# 37 unknown
+# packet_hosts_to_unquarantine = [49, 68, 7, 45, 37, 46]
+
+packet_hosts_to_unquarantine = [68]
+
 
 for h in packet_hosts_to_unquarantine:
   hosts_to_unquarantine.append("machine-%s" % h)
