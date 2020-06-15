532fa70855c02a7c569a8e32c45dc4e98aae13da
Andrew Erickson <aerickson@mozilla.com>
2020-06-15 14:45:14 -0700

    generate: add spaces to commented out lines

--- create_host_utils/generate_build_script.sh
+++ create_host_utils/generate_build_script.sh
@@ -24,15 +24,15 @@ set -e
 # - x86_64: 'Linux x64 opt' on treeherder
 # - win: 'Windows 2012 opt' on treeherder
 # - Mac: see https://wiki.mozilla.org/Packaging_Android_host_utilities#macOS
-#X86_TASKID=""
-#WIN_TASKID=""
-#MAC_BUILD_URL=""
+# X86_TASKID=""
+# WIN_TASKID=""
+# MAC_BUILD_URL=""
 
 # STEP 3: uncomment and run desired builds
 #
-#./create_new_host_utils_linux.sh \$X86_TASKID x86_64 "\$BUILD_SELECTED"
-#./create_new_host_utils_win.sh \$WIN_TASKID win "\$BUILD_SELECTED"
-#./create_new_host_utils_mac.sh "\$MAC_BUILD_URL"
+# ./create_new_host_utils_linux.sh \$X86_TASKID x86_64 "\$BUILD_SELECTED"
+# ./create_new_host_utils_win.sh \$WIN_TASKID win "\$BUILD_SELECTED"
+# ./create_new_host_utils_mac.sh "\$MAC_BUILD_URL"
 EOL
 
 chmod 755 $filename
