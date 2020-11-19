2fcc1494e1c01ee60262b1a60d5cd378bac78990
Andrew Erickson <aerickson@mozilla.com>
2020-11-18 17:59:44 -0800

    getcurrent: remove linux32 bit

--- create_host_utils/get_current.sh
+++ create_host_utils/get_current.sh
@@ -16,7 +16,6 @@ cd current
 # testing/config/tooltool-manifests/linux32/hostutils.manifest
 # testing/config/tooltool-manifests/linux64/hostutils.manifest
 # testing/config/tooltool-manifests/macosx64/hostutils.manifest
-# $TT_PATH fetch -m $mc_dir/testing/config/tooltool-manifests/linux32/hostutils.manifest
 "$TT_PATH" fetch -m "$mc_dir/testing/config/tooltool-manifests/linux64/hostutils.manifest"
 "$TT_PATH" fetch -m "$mc_dir/testing/config/tooltool-manifests/macosx64/hostutils.manifest"
 "$TT_PATH" fetch -m "$mc_dir/testing/config/tooltool-manifests/win32/hostutils.manifest"
