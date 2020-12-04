e836cbbcb2c87eba64f956f61716cb4ce45eac86
Andrew Erickson <aerickson@mozilla.com>
2020-12-04 14:54:32 -0800

    hostuils: compare: cleanup

--- create_host_utils/compare_versions.sh
+++ create_host_utils/compare_versions.sh
@@ -16,36 +16,6 @@ if [ -x "$(command -v compare2)" ]; then
   araxis_present=1
 fi
 
-# if [ -d ./hu_i686_* ]; then
-#   echo "i686: "
-#   echo ""
-#   echo "  " diff current/host-utils-*.en-US.linux-i686 hu_i686_*/host-utils-${FFVER}.en-US.linux-i686
-#   if [ "$araxis_present" -eq "1" ]; then
-#     echo "  " compare2 -swap current/host-utils-*.en-US.linux-i686 hu_i686_*/host-utils-${FFVER}.en-US.linux-i686
-#   fi
-#   echo ""
-# fi
-
-# if [ -d ./hu_x86_64_* ]; then
-#   echo "x86_64: "
-#   echo ""
-#   echo "  " diff current/host-utils-*.en-US.linux-x86_64 hu_x86_64_*/host-utils-$FFVER.en-US.linux-x86_64
-#   if [ "$araxis_present" -eq "1" ]; then
-#     echo "  " compare2 -swap current/host-utils-*.en-US.linux-x86_64 hu_x86_64_*/host-utils-$FFVER.en-US.linux-x86_64
-#   fi
-#   echo ""
-# fi
-
-# if [ -d ./hu_mac_* ]; then
-#   echo "Mac: "
-#   echo ""
-#   echo "  " diff current/host-utils-*.en-US.mac hu_mac_*/host-utils-$FFVER.en-US.mac
-#   if [ "$araxis_present" -eq "1" ]; then
-#     echo "  " compare2 -swap current/host-utils-*.en-US.mac hu_mac_*/host-utils-$FFVER.en-US.mac
-#   fi
-#   echo ""
-# fi
-
 archs=( "x86_64" "mac" "win32" )
 for arch in "${archs[@]}"; do
   for dir in "./hu_${arch}_"*; do
