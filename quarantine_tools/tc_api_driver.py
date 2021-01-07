ac837a68f8d2ba60597db3062edafea7a322a4fb
Andrew Erickson <aerickson@mozilla.com>
2021-01-06 17:20:30 -0800

    use a better pdb than ipdb

--- quarantine_tools/tc_api_driver.py
+++ quarantine_tools/tc_api_driver.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-import taskcluster
 import json
 import os
 import sys
+
+import pdb
+import taskcluster
 
 #
 # docs: https://pypi.org/project/taskcluster/
@@ -22,23 +24,22 @@ queue = taskcluster.Queue(
     {"rootUrl": "https://firefox-ci-tc.services.mozilla.com", "credentials": creds}
 )
 
-import ipdb
-
-ipdb.set_trace()
+pdb.set_trace()
 
 sys.exit()
 
-hosts_to_quarantine = []
-packet_hosts_to_quarantine = [4, 6, 10, 13, 14, 16, 43, 53, 54, 65, 68]
-
-for h in packet_hosts_to_quarantine:
-    hosts_to_quarantine.append("machine-%s" % h)
-
-for a_host in hosts_to_quarantine:
-    queue.quarantineWorker(
-        "terraform-packet",
-        "gecko-t-linux",
-        "packet-sjc1",
-        a_host,
-        {"quarantineUntil": taskcluster.fromNow("10 year")},
-    )
+#
+# hosts_to_quarantine = []
+# packet_hosts_to_quarantine = [4, 6, 10, 13, 14, 16, 43, 53, 54, 65, 68]
+#
+# for h in packet_hosts_to_quarantine:
+#     hosts_to_quarantine.append("machine-%s" % h)
+#
+# for a_host in hosts_to_quarantine:
+#     queue.quarantineWorker(
+#         "terraform-packet",
+#         "gecko-t-linux",
+#         "packet-sjc1",
+#         a_host,
+#         {"quarantineUntil": taskcluster.fromNow("10 year")},
+#     )
