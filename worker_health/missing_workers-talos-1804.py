a65053a128faaa3b00064b961bf6632de5980667
Andrew Erickson <aerickson@mozilla.com>
2021-02-10 15:07:05 -0800

    add quarantine tool, add pipenv for quarantine, disable mw talos 1804 for now

--- worker_health/missing_workers-talos-1804.py
+++ worker_health/missing_workers-talos-1804.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
 
 import fitness
 import quarantine
@@ -12,11 +13,14 @@ if __name__ == "__main__":
     PROVISIONER = "releng-hardware"
     WORKER_TYPE = "gecko-t-linux-talos-1804"
 
+    print("not implemented yet")
+    sys.exit(0)
+
     f = fitness.Fitness(log_level=0, provisioner=PROVISIONER, alert_percent=85)
     q = quarantine.Quarantine()
 
     q.print_quarantined_workers(PROVISIONER, WORKER_TYPE)
-    f.simple_worker_report(WORKER_TYPE, worker_count=70)
+    f.simple_worker_report(WORKER_TYPE, worker_count=12)
 
 
 # https://firefox-ci-tc.services.mozilla.com/provisioners/releng-hardware/worker-types/gecko-t-linux-talos-dw
