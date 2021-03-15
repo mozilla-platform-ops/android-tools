5f4f363bf8bcff651173042dbd20cbdb373f4884
Andrew Erickson <aerickson@mozilla.com>
2021-03-15 12:53:14 -0700

    tc_search: misc work (#26)

--- tc_search/README.md
+++ tc_search/README.md
@@ -5,9 +5,16 @@ Searches Taskcluster provisionerIds and workerTypes for a string.
 ## usage
 
 ```
+# install dependencies
 pip3 install poetry
 poetry shell
 
-./tc_search.py bitbar
+# show help
 ./tc_search.py -h
+
+# search for provisioners and worker types mentioning 'bitbar'
+./tc_search.py bitbar
+
+# list all provisioners and worker types
+./tc_search.py
 ```
--- tc_search/tc_search.py
+++ tc_search/tc_search.py
@@ -3,28 +3,32 @@
 import argparse
 import json
 import os
-import pprint
-import sys
+import textwrap
 
 import taskcluster
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    # parser.add_argument(
-    #     "-v",
-    #     "--verbose",
-    #     action="count",
-    #     default=False,
-    #     help="specify multiple times for even more verbosity",
-    # )
-    # parser.add_argument("-d", "--daemon-mode", action="store_true")
-    parser.add_argument("search_term", type=str, help="string to search for")
-    args = parser.parse_args()
+def fg(text, color):
+    return "\33[38;5;" + str(color) + "m" + text + "\33[0m"
 
-    # lambdas for colored text
-    fg = lambda text, color: "\33[38;5;" + str(color) + "m" + text + "\33[0m"
-    bg = lambda text, color: "\33[48;5;" + str(color) + "m" + text + "\33[0m"
+
+def bg(text, color):
+    return "\33[48;5;" + str(color) + "m" + text + "\33[0m"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        epilog=textwrap.dedent(
+            """\
+        If the search term is omitted, all provisioners and
+        worker types will be listed.
+        """
+        )
+    )
+    parser.add_argument(
+        "search_term", type=str, nargs="?", help="string to search for", default=None
+    )
+    args = parser.parse_args()
 
     search_term = args.search_term
 
@@ -38,26 +42,46 @@ if __name__ == "__main__":
     p_count = 0
     wt_count = 0
     m_count = 0
-    for item in queue.listProvisioners()["provisioners"]:
-        p_count += 1
-        provisioner_id = item["provisionerId"]
-        if search_term in provisioner_id:
-            m_count += 1
+
+    if not search_term:
+        for item in queue.listProvisioners()["provisioners"]:
+            p_count += 1
+            provisioner_id = item["provisionerId"]
             print(
                 "https://firefox-ci-tc.services.mozilla.com/provisioners/%s"
                 % fg(provisioner_id, 13)
             )
-        for wt in queue.listWorkerTypes(provisioner_id)["workerTypes"]:
-            wt_count += 1
-            worker_type = wt["workerType"]
-            # import pdb; pdb.set_trace()
-            if search_term in worker_type:
-                m_count += 1
+            for wt in queue.listWorkerTypes(provisioner_id)["workerTypes"]:
+                wt_count += 1
+                worker_type = wt["workerType"]
+                # import pdb; pdb.set_trace()
                 print(
-                    "https://firefox-ci-tc.services.mozilla.com/provisioners/%s/worker-types/%s"
+                    "  https://firefox-ci-tc.services.mozilla.com/provisioners/%s/worker-types/%s"
                     % (provisioner_id, fg(worker_type, 14))
                 )
-    print(
-        "%s matches, scanned %s provisionerIds and %s workerTypes"
-        % (m_count, p_count, wt_count)
-    )
+        print("found %s provisionerIds and %s workerTypes" % (p_count, wt_count))
+
+    else:
+        for item in queue.listProvisioners()["provisioners"]:
+            p_count += 1
+            provisioner_id = item["provisionerId"]
+            if search_term in provisioner_id:
+                m_count += 1
+                print(
+                    "https://firefox-ci-tc.services.mozilla.com/provisioners/%s"
+                    % fg(provisioner_id, 13)
+                )
+            for wt in queue.listWorkerTypes(provisioner_id)["workerTypes"]:
+                wt_count += 1
+                worker_type = wt["workerType"]
+                # import pdb; pdb.set_trace()
+                if search_term in worker_type:
+                    m_count += 1
+                    print(
+                        "https://firefox-ci-tc.services.mozilla.com/provisioners/%s/worker-types/%s"
+                        % (provisioner_id, fg(worker_type, 14))
+                    )
+        print(
+            "%s matches, scanned %s provisionerIds and %s workerTypes"
+            % (m_count, p_count, wt_count)
+        )
