ca765b6cd6283f35111314bdb9b5d403ef19bf8c
Andrew Erickson <aerickson@mozilla.com>
2021-05-12 15:20:30 -0700

    missing_workers_1804 works

--- worker_health/missing_workers-talos-1804.py
+++ worker_health/missing_workers-talos-1804.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
 
 import argparse
-import sys
 
 import fitness
 import quarantine
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        dest="log_level",
+        default=0,
+        help="specify multiple times for even more verbosity.",
+    )
     args = parser.parse_args()
 
     PROVISIONER = "releng-hardware"
     WORKER_TYPE = "gecko-t-linux-talos-1804"
 
-    print("not implemented yet")
-    sys.exit(0)
+    exclude_arr = [
+        "t-linux64-ms-055",  # bad ilo on cart
+        "t-linux64-ms-228",  # nvme error
+    ]
+
+    # print("not implemented yet")
+    # sys.exit(0)
 
     f = fitness.Fitness(log_level=0, provisioner=PROVISIONER, alert_percent=85)
     q = quarantine.Quarantine()
 
     q.print_quarantined_workers(PROVISIONER, WORKER_TYPE)
-    f.simple_worker_report(WORKER_TYPE, worker_count=12)
+    f.moonshot_worker_report(WORKER_TYPE, args=args, exclude_arr=exclude_arr)
 
 
 # https://firefox-ci-tc.services.mozilla.com/provisioners/releng-hardware/worker-types/gecko-t-linux-talos-dw
