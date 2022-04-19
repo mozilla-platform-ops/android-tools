9412be0498eea276d8a2b86fcaccf1f3c50ab0a4
Andrew Erickson <aerickson@mozilla.com>
2022-04-19 14:01:21 -0400

    wip 2

--- quarantine_tools/tc_quarantine.py
+++ quarantine_tools/tc_quarantine.py
@@ -1,3 +1,9 @@
+import json
+import os
+
+import taskcluster
+
+
 class TCQuarantine:
     def __init__(
         self,
@@ -18,12 +24,23 @@ class TCQuarantine:
         self.padding = padding
         self.padding_length = padding_length
 
+        # load creds and set queue object
+        with open(os.path.expanduser("~/.tc_quarantine_token")) as json_file:
+            data = json.load(json_file)
+        creds = {"clientId": data["clientId"], "accessToken": data["accessToken"]}
+
+        self.queue = taskcluster.Queue(
+            {
+                "rootUrl": "https://firefox-ci-tc.services.mozilla.com",
+                "credentials": creds,
+            }
+        )
+
     def generate_hosts(self, device_numbers):
         hosts_to_act_on = []
         for h in device_numbers:
             if self.padding:
                 if self.padding_length == 3:
-                    # t-linux64-ms-
                     hosts_to_act_on.append("%s%03d" % (self.worker_name_root, h))
                 else:
                     raise "not implemented yet!"
@@ -34,10 +51,31 @@ class TCQuarantine:
         pass
 
     def lift_quarantine(self, device_numbers):
-        # calls quarantine with a date in the past
-        pass
+        for a_host in device_numbers:
+            print("removing %s from quarantine... " % a_host)
+            try:
+                self.queue.quarantineWorker(
+                    "releng-hardware",
+                    "gecko-t-linux-talos",
+                    "mdc1",
+                    a_host,
+                    {"quarantineUntil": taskcluster.fromNow("-1 year")},
+                )
+            except taskcluster.exceptions.TaskclusterRestFailure as e:
+                # usually due to worker not being in pool...
+                # TODO: inspect message
+                print(e)
 
 
 BitbarP2UnitQuarantine = TCQuarantine(
-    "proj-autophone", "gecko-t-bitbar-gw-unit-p2", "bitbar"
+    "proj-autophone", "gecko-t-bitbar-gw-unit-p2", "bitbar", "pixel2-"
+)
+
+Talos1804Quarantine = TCQuarantine(
+    "releng-hardware",
+    "gecko-t-linux-talos",
+    "mdc1",
+    "t-linux64-ms-",
+    padding=True,
+    padding_length=3,
 )
