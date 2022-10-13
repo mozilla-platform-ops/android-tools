bd874739965f7408a4a327268f1bffbd6ce443b1
Andrew Erickson <aerickson@mozilla.com>
2022-10-13 16:04:42 -0700

    seems to be working

--- worker_health/worker_health/status.py
+++ worker_health/worker_health/status.py
@@ -37,7 +37,7 @@ class Status:
             {"rootUrl": self.root_url, "credentials": creds}
         )
 
-    def jobs_running(self, provisioner, worker_type, hosts):
+    def jobs_running(self, provisioner, worker_group, worker_type, hosts):
         tch = tc_helpers.TCHelper(provisioner=provisioner)
         # issue: doesn't show state (running or idle)
         # pprint.pprint(f.get_workers(worker_type))
@@ -45,9 +45,26 @@ class Status:
         if len(hosts) == 0:
             raise Exception("no hosts specified")
 
+        hosts_with_non_completed_or_failed_jobs = []
         for host in hosts:
-            # TODO: don't hardcode mdc1
-            pprint.pprint(tch.get_worker_jobs(worker_type, "mdc1", host))
+            results = tch.get_worker_jobs(worker_type, worker_group, host)
+            # pprint.pprint(results)
+            for result in results["recentTasks"]:
+                task_id = result["taskId"]
+                # pprint.pprint(task_id)
+                _tid, status_blob, _exc = tch.get_task_status(task_id)
+                # pprint.pprint(status_blob)
+                t_status = status_blob["status"]["state"]
+                if t_status != "completed" and t_status != "failed":
+                    pprint.pprint(status_blob["status"]["state"])
+                    hosts_with_non_completed_or_failed_jobs.append(host)
+            # for job in jobs:
+            # see if running, if yes, return true or accumulate
+
+        print(
+            f"hosts_with_non_completed_or_failed_jobs: {hosts_with_non_completed_or_failed_jobs}"
+        )
+        # pprint.pprint(hosts_with_non_completed_or_failed_jobs)
 
     def list_workers(self, provisioner, worker_type):
         results = self.tc_wm.listWorkers(worker_type, provisioner)
@@ -60,17 +77,18 @@ if __name__ == "__main__":
     si = Status()
     si.jobs_running(
         "releng-hardware",
+        "mdc1",
         "gecko-t-osx-1015-r8",
         [
             "macmini-r8-1",
-            "macmini-r8-2",
-            "macmini-r8-3",
-            "macmini-r8-4",
-            "macmini-r8-5",
-            "macmini-r8-7",
-            "macmini-r8-8",
-            "macmini-r8-9",
-            "macmini-r8-10",
+            # "macmini-r8-2",
+            # "macmini-r8-3",
+            # "macmini-r8-4",
+            # "macmini-r8-5",
+            # "macmini-r8-7",
+            # "macmini-r8-8",
+            # "macmini-r8-9",
+            # "macmini-r8-10",
         ],
     )
     # import ipdb; ipdb.set_trace()
--- worker_health/worker_health/tc_helpers.py
+++ worker_health/worker_health/tc_helpers.py
@@ -18,5 +18,13 @@ class TCHelper:
             "https://firefox-ci-tc.services.mozilla.com/api/queue/v1/provisioners/%s/worker-types/%s/workers/%s/%s"
             % (self.provisioner, queue, worker_type, worker)
         )
-        print(url)
+        # print(url)
         return utils.get_jsonc(url, self.log_level)
+
+    def get_task_status(self, taskid):
+        _url, output, exception = utils.get_jsonc2(
+            "https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/%s/status"
+            % taskid
+            # "https://queue.taskcluster.net/v1/task/%s/status" % taskid
+        )
+        return taskid, output, exception
