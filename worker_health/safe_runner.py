ccff41ddcbfd5623c4081aa8763bfb10cdb1b1b6
Andrew Erickson <aerickson@mozilla.com>
2022-10-25 16:44:44 -0700

    sr: checkpoint

--- worker_health/safe_runner.py
+++ worker_health/safe_runner.py
@@ -6,6 +6,7 @@
 
 import argparse
 import re
+import subprocess
 import sys
 
 from worker_health import quarantine, status
@@ -36,10 +37,16 @@ class SafeRunner:
         self.provisioner = provisioner
         self.worker_type = worker_type
 
-        self.si = status.Status()
+        self.si = status.Status(provisioner, worker_type)
         self.q = quarantine.Quarantine()
 
-    def safe_run(self, hostname, command, verbose=True):
+    # TODO: have a multi-host with smarter sequencing...
+    #   - for large groups of hosts, quarantine several at a time?
+
+    def safe_run_single_host(self, hostname, command, verbose=True):
+        # TODO: ensure command has SR_HOST variable in it
+
+        # quarantine
         if verbose:
             print(f"{hostname}: adding to quarantine...")
         self.q.quarantine(self.provisioner, self.worker_type, [hostname])
@@ -47,13 +54,42 @@ class SafeRunner:
         if verbose:
             print(f"{hostname}: quarantined.")
 
-        # TODO: wait until drained (not running jobs)
-        # self.si.
+        # wait until drained (not running jobs)
+        if verbose:
+            print(f"{hostname}: waiting for host to drain...")
+        self.si.wait_until_no_jobs_running([hostname])
+        if verbose:
+            print(f"{hostname}: drained.")
 
-        # TODO: run command
+        # run command
+        custom_cmd = command.replace("SR_HOST", hostname)
+        if verbose:
+            print(f"{hostname}: running command '{custom_cmd}'...")
+        split_custom_cmd = ["/bin/bash", "-l", "-c", custom_cmd]
+        ro = subprocess.run(
+            split_custom_cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE
+        )
+        rc = ro.returncode
 
-        # TODO: lift quarantine
+        print("")
+        output = ro.stdout.decode()
+        for line in output:
+            print(line)
+        print("")
 
+        if rc != 0:
+            print(f"{hostname}: command failed. exiting...")
+            sys.exit(1)
+        if verbose:
+            print(f"{hostname}: command completed successfully.")
+
+        # lift quarantine
+        if verbose:
+            print(f"{hostname}: lifting quarantine...")
+        self.q.lift_quarantine(self.provisioner, self.worker_type, [hostname])
+        # TODO: verify?
+        if verbose:
+            print(f"{hostname}: quarantine lifted.")
         pass
 
 
@@ -66,19 +102,26 @@ if __name__ == "__main__":
     args = parser.parse_args()
     args.hosts = args.host_csv
 
-    print(args)
-    sys.exit(0)
+    # print(args)
+    # sys.exit(0)
 
     sr = SafeRunner(args.provisioner, args.worker_type)
 
     # get user to ack what we're about to do
     print("about to do the following:")
-    print("type 'yes' to continue: ")
+    print(f"  provisioner: {args.provisioner}")
+    print(f"  worker_type: {args.worker_type}")
+    print(f"  hosts: {args.hosts}")
+    print(f"  command: {args.command}")
+    print("type 'yes' to continue: ", end="")
     user_input = input()
     if user_input != "yes":
         print("user chose to exit")
         sys.exit(0)
 
+    host_total = len(args.hosts)
+    counter = 0
     for host in args.hosts:
-        print(f"{host}")
-        sr.safe_run(host, args.command)
+        counter += 1
+        print(f"*** {counter}/{host_total}: {host}")
+        sr.safe_run_single_host(host, args.command)
