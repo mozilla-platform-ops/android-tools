47a816643ea1123c6a1979addcdf73f63e523354
Andrew Erickson <aerickson@mozilla.com>
2023-10-12 13:31:19 -0700

    safe_runner: add tests and comment (#96)

--- worker_health/tests/test_runner.py
+++ worker_health/tests/test_runner.py
@@ -34,4 +34,24 @@ def test_get_host_counts_general(datadir):
 def test_get_host_counts_general2(datadir):
     confdir = datadir.join("general2")
     r = runner.Runner.from_resume(confdir, test_mode=True)
-    assert r.get_host_counts() == {"skipped": 1, "completed": 1, "failed": 1, "remaining": 4, "total": 7, "to_skip": 1}
+    assert r.get_host_counts() == {"skipped": 1, "completed": 1, "failed": 1, "remaining": 4, "total": 7, "to_skip": 2}
+
+
+def test_load_config(datadir):
+    confdir = datadir.join("general")
+    r = runner.Runner.from_resume(confdir, test_mode=True)
+    assert r.provisioner == "releng-hardware"
+    assert r.worker_type == "gecko-t-osx-1015-r8"
+    assert r.command == "ssh SR_HOST.SR_FQDN echo monkey"
+    assert r.hosts_to_skip == ["macmini-r8-9"]
+    assert r.fqdn_postfix == "test.releng.mdc1.mozilla.com"
+
+
+def test_load_config2(datadir):
+    confdir = datadir.join("general2")
+    r = runner.Runner.from_resume(confdir, test_mode=True)
+    assert r.provisioner == "releng-hardware999"
+    assert r.worker_type == "gecko-t-banana"
+    assert r.command == "ssh SR_HOST.SR_FQDN echo FUN FUN FUN"
+    assert r.hosts_to_skip == ["macmini-r8-222", "macmini-r8-391"]
+    assert r.fqdn_postfix == "test.releng.mdc1.mozilla.comzzz"
