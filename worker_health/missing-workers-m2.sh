7072810ef5defa203341fdb0ed764f1ed3fa66fa
Andrew Erickson <aerickson@mozilla.com>
2024-03-08 12:09:09 -0800

    m2: make more generic

--- worker_health/missing-workers-m2.sh
+++ worker_health/missing-workers-m2.sh
@@ -7,4 +7,4 @@ echo ""
 echo "WARNING: report is likely wrong for amazon ec2 and bare IP addresses"
 echo ""
 
-./missing-workers-r8.py -i /Users/aerickson/git/ronin_puppet/inventory.d/macmini-m2.yaml
+./missing-workers-r8.py -i /Users/"$USER"/git/ronin_puppet/inventory.d/macmini-m2.yaml
