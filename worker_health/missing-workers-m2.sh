c5870ebea9d7991fb95194f396fbe4dde8c956c3
Andrew Erickson <aerickson@mozilla.com>
2024-03-08 12:05:07 -0800

    note in m2 script

--- worker_health/missing-workers-m2.sh
+++ worker_health/missing-workers-m2.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
+# set -x
+
+echo ""
+echo "WARNING: report is likely wrong for amazon ec2 and bare IP addresses"
+echo ""
 
 ./missing-workers-r8.py -i /Users/aerickson/git/ronin_puppet/inventory.d/macmini-m2.yaml
