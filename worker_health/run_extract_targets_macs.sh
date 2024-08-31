a263ec116b6a15eab372798f2d2a08e62a0a5448
Andrew Erickson <aerickson@mozilla.com>
2024-08-30 17:17:06 -0700

    add note

--- worker_health/run_extract_targets_macs.sh
+++ worker_health/run_extract_targets_macs.sh
@@ -4,6 +4,7 @@ set -e
 
 RONIN_INVENTORY_DIR=$HOME/git/ronin_puppet/inventory.d
 
+# this excludes signing hosts, as they require duo auth and runner fails with them
 ./extract_targets.py \
   "$RONIN_INVENTORY_DIR"/macmini-m1.yaml \
   "$RONIN_INVENTORY_DIR"/macmini-m2.yaml \
