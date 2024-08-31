c5dbe383fcea0f52b7c1cf8ac30d9fa4ccd4f845
Andrew Erickson <aerickson@mozilla.com>
2024-08-30 17:14:22 -0700

    use $HOME

--- worker_health/run_extract_targets_macs.sh
+++ worker_health/run_extract_targets_macs.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-RONIN_INVENTORY_DIR=/Users/aerickson/git/ronin_puppet/inventory.d
+RONIN_INVENTORY_DIR=$HOME/git/ronin_puppet/inventory.d
 
 ./extract_targets.py \
-  $RONIN_INVENTORY_DIR/macmini-m1.yaml \
-  $RONIN_INVENTORY_DIR/macmini-m2.yaml \
-  $RONIN_INVENTORY_DIR/macmini-r8.yaml \
-  $RONIN_INVENTORY_DIR/services.yaml \
+  "$RONIN_INVENTORY_DIR"/macmini-m1.yaml \
+  "$RONIN_INVENTORY_DIR"/macmini-m2.yaml \
+  "$RONIN_INVENTORY_DIR"/macmini-r8.yaml \
+  "$RONIN_INVENTORY_DIR"/services.yaml \
    -r "$@"
