d5879077cbc4d26847ef9ee6a1544353d4289a23
Andrew Erickson <aerickson@mozilla.com>
2024-10-17 23:12:56 -0500

    improve help, add notes

--- worker_health/run_extract_targets_macs.sh
+++ worker_health/run_extract_targets_macs.sh
@@ -4,10 +4,16 @@ set -e
 
 RONIN_INVENTORY_DIR=$HOME/git/ronin_puppet/inventory.d
 
+# notes:
+#   - `-r` mode takes a sr/ur resume dir as an argument and
+#      updates `remaining_hosts` in the state file.
+#   - `-t` mode outputs the remaining_hosts key/value as text
+
 # this excludes signing hosts, as they require duo auth and runner fails with them
 ./extract_targets.py \
   "$RONIN_INVENTORY_DIR"/macmini-m1.yaml \
   "$RONIN_INVENTORY_DIR"/macmini-m2.yaml \
   "$RONIN_INVENTORY_DIR"/macmini-r8.yaml \
   "$RONIN_INVENTORY_DIR"/services.yaml \
-   -r "$@"
+  -r "$@"
+  # -t "$@"
