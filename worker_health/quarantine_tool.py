3c0fe1150b3ebae9c93cf83c002e2f6b619f7c7c
Andrew Erickson <aerickson@mozilla.com>
2025-05-14 18:00:43 -0700

    comment cleanup

--- worker_health/quarantine_tool.py
+++ worker_health/quarantine_tool.py
@@ -111,9 +111,6 @@ if __name__ == "__main__":
                 pprint.pprint(quarantine_info)
             else:
                 print(", ".join(formatted_workers))
-            # human order
-            # results.sort(key=lambda d: "{0:0>8}".format(d.replace("macmini-r8-", "")))
-            # print(",".join(results))
     elif args.action == "show-all":
         q = quarantine.Quarantine()
         results = q.get_workers(args.provisioner, args.worker_type)
